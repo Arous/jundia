8154b690b01e6d8cd083f64f00e1310fac53d8fc
Arous <myunani@naver.com>
2025-02-08 21:38:45 +0900

    chore: remove unused code

--- src/pages/shows/Rainbow/useRainbowPhoto.ts
+++ src/pages/shows/Rainbow/useRainbowPhoto.ts
@@ -6,49 +6,6 @@ interface ImageInfo {
 
 export const breakpoints = [3840, 2400, 1080, 640, 384, 256, 128, 96, 64, 48];
 
-// const imagesFileNames = [
-//   '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.png', '9.png'
-// ]
-
-// const imagePath = "/assets/images_compressed/Circus/Show_Rainbow";
-
-// const images1 = imagesFileNames.map((fileName) => {
-//   return `${imagePath}/${fileName}`
-// });
-
-
-// const getImageDim = (url: string): Promise<{width: number, height: number}> => {
-//   return new Promise((resolve, reject) => {
-//     const img = new Image();
-//     img.onload = () => resolve({
-//       width: img.width,
-//       height: img.height,
-//     });
-//     img.onerror = (error) => reject(error);
-//     img.src = url;
-//   });
-// };
-
-
-// export const loadCircusImages = async(): Promise<ImageInfo[]> => {
-//   console.log(images1)
-
-//   const circusImagesPromises = images1.map(async (path) => {
-//     const { width, height } = await getImageDim(path);
-
-//     return {
-//       src: path,
-//       width: width,
-//       height: height,
-//     }
-//   });
-
-
-//   const circusImages = await Promise.all(circusImagesPromises);
-
-//   return circusImages
-// }
-
 
 const IMAGE_PATHS = {
     a: "/assets/images_compressed/Circus/Show_Rainbow",

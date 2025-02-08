d2cc71a2eb601f7d816ae6af2d79c06b1ea35cd9
Arous <myunani@naver.com>
2025-02-08 20:00:52 +0900

    chore: update `shows/Butterfly`

--- src/pages/shows/Butterfly/Butterfly.tsx
+++ src/pages/shows/Butterfly/Butterfly.tsx
@@ -38,9 +38,10 @@ const Butterfly = () => {
         <iframe
           width="100%"
           height="100%"
-          src="https://www.youtube.com/embed/O04ImFPQqH8"
+          src="https://www.youtube.com/embed/DO-zChKRGCw?si=jBlN-vFtkc_ScEe0"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
         ></iframe>
       </div>
 
--- src/pages/shows/Butterfly/ButterflyGallery.tsx
+++ src/pages/shows/Butterfly/ButterflyGallery.tsx
@@ -1,19 +1,94 @@
+// import { useEffect, useState } from "react";
+
+// import PhotoAlbum from "react-photo-album";
+
+// import Lightbox from "yet-another-react-lightbox";
+// import "yet-another-react-lightbox/styles.css";
+
+// // import optional lightbox plugins
+// import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+// import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+// import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+// import Zoom from "yet-another-react-lightbox/plugins/zoom";
+// import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+// import { loadCircusImages, breakpoints } from "./useButterflyPhoto";
+
+// type Photo = {
+//   src: string;
+//   width: number;
+//   height: number;
+//   images: {
+//     src: string;
+//     width: number;
+//     height: number;
+//   }[];
+// }
+
+// export default function Gallery() {
+//   const [index, setIndex] = useState(-1);
+//   const [photos, setPhotos] = useState<Photo[]>([]);
+
+//   useEffect(() => {
+//     const fetchImages = async () => {
+//       const images = await loadCircusImages();
+
+//       const formattedPhotos = images.map((photo) => {
+//         const width = breakpoints[1];
+//         const height = (photo.height / photo.width) * width;
+
+//         return {
+//           src: photo.src,
+//           width,
+//           height,
+//           images: breakpoints.map((breakpoint) => {
+//             const height = Math.round((photo.height / photo.width) * breakpoint);
+
+//             return {
+//               src: photo.src,
+//               width: breakpoint,
+//               height,
+//             };
+//           }),
+//         };
+//       });
+
+//       setPhotos(formattedPhotos);
+//     };
+
+//     fetchImages();
+//   }, []);
+
+//   return (
+//       <>
+//           <PhotoAlbum photos={photos} layout="rows" targetRowHeight={150} onClick={({ index }) => setIndex(index)} />
+
+//           <Lightbox
+//               slides={photos}
+//               open={index >= 0}
+//               index={index}
+//               close={() => setIndex(-1)}
+//               // enable optional lightbox plugins
+//               plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+//           />
+//       </>
+//   );
+// }
+
 import { useEffect, useState } from "react";
 
 import PhotoAlbum from "react-photo-album";
-
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
 
-// import optional lightbox plugins
+// Optional lightbox plugins
 import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
 import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
-import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
 
-import { loadCircusImages, breakpoints } from "./useButterflyPhoto";
-
+import { breakpoints, loadAllImages } from "./useButterflyPhoto";
 
 type Photo = {
   src: string;
@@ -24,54 +99,78 @@ type Photo = {
     width: number;
     height: number;
   }[];
-}
+};
+
+/**
+ * Helper function to convert a raw image (with known dimensions)
+ * into a formatted photo object that includes responsive image variants.
+ */
+const formatPhoto = (img: {
+  src: string;
+  width: number;
+  height: number;
+}): Photo => {
+  // Use the second breakpoint as the default width.
+  const defaultWidth = breakpoints[1];
+  const defaultHeight = (img.height / img.width) * defaultWidth;
+
+  const responsiveVariants = breakpoints.map((bp) => {
+    const variantHeight = Math.round((img.height / img.width) * bp);
+    return {
+      src: img.src,
+      width: bp,
+      height: variantHeight,
+    };
+  });
+
+  return {
+    src: img.src,
+    width: defaultWidth,
+    height: defaultHeight,
+    images: responsiveVariants,
+  };
+};
 
 export default function Gallery() {
-  const [index, setIndex] = useState(-1);
+  // State to control the index of the open slide in the lightbox.
+  const [lightboxIndex, setLightboxIndex] = useState<number>(-1);
+  // Gallery photos formatted for PhotoAlbum and Lightbox.
   const [photos, setPhotos] = useState<Photo[]>([]);
 
   useEffect(() => {
-    const fetchImages = async () => {
-      const images = await loadCircusImages();
-
-      const formattedPhotos = images.map((photo) => {
-        const width = breakpoints[1];
-        const height = (photo.height / photo.width) * width;
-
-        return {
-          src: photo.src,
-          width,
-          height,
-          images: breakpoints.map((breakpoint) => {
-            const height = Math.round((photo.height / photo.width) * breakpoint);
-
-            return {
-              src: photo.src,
-              width: breakpoint,
-              height,
-            };
-          }),
-        };
-      });
-
-      setPhotos(formattedPhotos);
+    const loadGalleryImages = async () => {
+      try {
+        // Load both image sets concurrently.
+        const { circus, butterfly } = await loadAllImages();
+        // Merge the two sets.
+        const allImages = [...circus, ...butterfly];
+        // Format each image to include responsive variants.
+        const formattedPhotos = allImages.map(formatPhoto);
+        setPhotos(formattedPhotos);
+      } catch (error) {
+        console.error("Failed to load images:", error);
+      }
     };
 
-    fetchImages();
+    loadGalleryImages();
   }, []);
 
   return (
-      <>
-          <PhotoAlbum photos={photos} layout="rows" targetRowHeight={150} onClick={({ index }) => setIndex(index)} />
+    <>
+      <PhotoAlbum
+        photos={photos}
+        layout="rows"
+        targetRowHeight={150}
+        onClick={({ index }) => setLightboxIndex(index)}
+      />
 
-          <Lightbox
-              slides={photos}
-              open={index >= 0}
-              index={index}
-              close={() => setIndex(-1)}
-              // enable optional lightbox plugins
-              plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
-          />
-      </>
+      <Lightbox
+        slides={photos}
+        open={lightboxIndex >= 0}
+        index={lightboxIndex}
+        close={() => setLightboxIndex(-1)}
+        plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+      />
+    </>
   );
 }
